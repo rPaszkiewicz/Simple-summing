f22a84c6c0282f25cde859c4332e49ee57cb9346
Dev Mąż <yakuzatanaka@gmail.com>
2019-03-21 13:00:07 +0100

    SPOJ PA-05

--- src/com/justRandomStuff/Main.java
+++ src/com/justRandomStuff/Main.java
@@ -7,21 +7,69 @@ public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        System.out.println("Word to encrypt or decrypt:");
-        String word = reader.nextLine();
-        rot13(word);
-    }
-    public static void rot13(String input) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
-            if       (c >= 'a' && c <= 'm') c += 13;
-            else if  (c >= 'A' && c <= 'M') c += 13;
-            else if  (c >= 'n' && c <= 'z') c -= 13;
-            else if  (c >= 'N' && c <= 'Z') c -= 13;
-            sb.append(c);
-        }
-        System.out.println("Message: " + input + " encrypted to: " + sb.toString());
+        System.out.println("Podaj podstawę");
+        int num = reader.nextInt();
+        System.out.println("Podaj wykładnik");
+        int num1 = reader.nextInt();
+
+        powering(num,num1);
     }
 
+    public static void powering(int num, int num1){
+        int pow = (int)Math.pow(num,num1);
+        StringBuilder sb = new StringBuilder().append(pow);
+
+        System.out.println(sb.substring(sb.length()-1));
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
