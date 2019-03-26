0430c9bb091193d0a74fa0b337ddef0051afb926
Dev Mąż <yakuzatanaka@gmail.com>
2019-03-26 09:48:27 +0100

    summing numbers without whitespace

--- src/com/justRandomStuff/Main.java
+++ src/com/justRandomStuff/Main.java
@@ -1,36 +1,28 @@
 package com.justRandomStuff;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
+
         Scanner reader = new Scanner(System.in);
+        System.out.println("Please enter numbers to sum:");
 
-        System.out.println("Podaj podstawę");
-        int num = reader.nextInt();
-        System.out.println("Podaj wykładnik");
-        int num1 = reader.nextInt();
-
-        powering(num,num1);
-    }
-
-    public static void powering(int num, int num1){
-        int pow = (int)Math.pow(num,num1);
-        StringBuilder sb = new StringBuilder().append(pow);
-
-        System.out.println(sb.substring(sb.length()-1));
+        while(reader.hasNextLine()) {
+            String num = reader.nextLine();
+            String num1 = num.replaceAll("\\s","");
+            int sum =0;
+            if (num.length()<2)
+                continue;
+            for (int i=0; i<num1.length(); i++) {
+                sum+=Character.getNumericValue(num1.charAt(i));
+            }
+            System.out.println(sum);
+            //break;
+        }
     }
 }
-
-
-
-
-
-
-
-
-
 
 
 
